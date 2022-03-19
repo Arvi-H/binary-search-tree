7209ccbf15ae05c6f1c01e871079eae46a508fa8
Arvi-H <arvihaxhillari1@gmail.com>
2022-03-18 23:48:15 -0600

    Created node struct

--- BST.h
+++ BST.h
@@ -1,6 +1,24 @@
-#ifndef BST 
-#define BST 
+#include "BSTInterface.h"
 
+using namespace std;
 
+template <typename T>
 
-#endif
+class BST : public BSTInterface <T> {
+private:
+    struct Node {
+        // Current node data
+        T data;
+
+        // Data less than current node
+        T* left;
+
+        // Data greater than current node
+        T* right;
+
+        // Node constructor
+        Node(T d) : data(d), left(NULL), right(NULL) {}
+    };
+
+};
+ 
