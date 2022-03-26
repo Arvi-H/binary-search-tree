31768f1ba01b7d3e8b2374b65d94d64290d00be4
Arvi-H <arvihaxhillari1@gmail.com>
2022-03-26 11:19:18 -0600

    Added necessary functions for our BST

--- BST.h
+++ BST.h
@@ -7,18 +7,38 @@ template <typename T>
 class BST : public BSTInterface <T> {
 private:
     struct Node {
-        // Current node data
-        T data;
-
-        // Data less than current node
-        T* left;
-
-        // Data greater than current node
-        T* right;
+        T data; // Current node data
+        T* left; // Data less than current node
+        T* right; // Data greater than current node
 
         // Node constructor
         Node(T d) : data(d), left(NULL), right(NULL) {}
     };
+
+public:
+    BST() {}
+    ~BST() {}
+    
+    // Return true if able to add node else return false
+    bool addNode(const T&) {}
+    
+    // Return true when node is removed else return false
+    bool removeNode(const T&) {}
+    
+    // Return true if tree is empty else return false
+    bool clearTree() {}
+    
+    // Return a level order traversal of a BST as a string
+    string toString() const {}
+
+    // Delete all nodes from BST
+    void clear(void) {}
+ 
+    // Return the number of nodes in the BST
+    size_t size(void) {}
+
+    // Return "Found" if able to find not in BST else return "Not Found"
+    string find(void) {}
 
 };
  
